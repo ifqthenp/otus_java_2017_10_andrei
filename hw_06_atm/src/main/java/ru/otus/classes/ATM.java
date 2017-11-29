fb2842b3accbbf37b1daf05bc9080432cd34f6d9
ifqthenp <ifqthenp@gmail.com>
2017-11-29 17:01:13 +0000

    Fix main class property

---
+++ hw_06_atm/src/main/java/ru/otus/classes/ATM.java
@@ -0,0 +1,63 @@
+package ru.otus.classes;
+
+import ru.otus.interfaces.AtmOperations;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+/**
+ * {@code ATM} class.
+ */
+public class ATM implements AtmOperations
+{
+    private static final long ID = 1001L;
+    private ATM.Cash atmCash;
+
+    private ATM(final CashBuilder cashBuilder)
+    {
+        this.atmCash = new ATM.Cash(cashBuilder);
+    }
+
+    static ATM loadCash(CashBuilder cashBuilder)
+    {
+        return new ATM(cashBuilder);
+    }
+
+    public Map<Integer, Integer> getCash()
+    {
+        return this.atmCash.getCash();
+    }
+
+    @Override
+    public void withdraw(final int amount)
+    {
+
+    }
+
+    @Override
+    public int getCashTotal()
+    {
+        return atmCash.getTotal();
+    }
+
+    private static class Cash
+    {
+        private Map<Integer, Integer> cash;
+
+        Cash(final CashBuilder cashBuilder)
+        {
+            this.cash = cashBuilder.getCashMap();
+        }
+
+        Map<Integer, Integer> getCash()
+        {
+            return this.cash;
+        }
+
+        int getTotal()
+        {
+            return this.cash.keySet().stream()
+                .reduce(0, Integer::sum);
+        }
+    }
+}
