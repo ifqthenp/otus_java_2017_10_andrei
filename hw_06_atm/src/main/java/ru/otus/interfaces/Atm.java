22550f52ec2d1eb0fa5ca02c25d8d110daf99842
ifqthenp <ifqthenp@gmail.com>
2017-12-01 13:11:32 +0000

    Add interface for cash machine

--- hw_06_atm/src/main/java/ru/otus/interfaces/Atm.java
+++ hw_06_atm/src/main/java/ru/otus/interfaces/Atm.java
@@ -1,20 +1,21 @@
 package ru.otus.interfaces;
 
 /**
- * {@code AtmMachine} describes operations allowed for an ATM.
+ * {@code Atm} describes operations allowed for an Automated
+ * Teller Machine (ATM) that implements this interface.
  */
-public interface AtmMachine
+public interface Atm
 {
     /**
-     * Allows user to withdraw given amount of cash from an ATM. The exact
-     * amount of cash available for withdrawal is subject to ATM implementation.
+     * Allows user to withdraw given amount of cash from an CashMachine. The exact
+     * amount of cash available for withdrawal is subject to CashMachine implementation.
      *
      * @param amount amount of cash to withdraw
      */
     void withdraw(int amount);
 
     /**
-     * Gets total amount of cash available in ATM.
+     * Gets total amount of cash available in CashMachine.
      *
      * @return total amount of cash
      */
