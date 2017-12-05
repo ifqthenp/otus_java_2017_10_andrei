76f1ab72c274c98cfa18552e0f690b4f0e7b795a
ifqthenp <ifqthenp@gmail.com>
2017-12-05 12:16:41 +0000

    Improve javadoc

--- hw_06_atm/src/main/java/ru/otus/interfaces/Atm.java
+++ hw_06_atm/src/main/java/ru/otus/interfaces/Atm.java
@@ -9,15 +9,16 @@ import java.util.SortedMap;
 public interface Atm
 {
     /**
-     * Allows user to withdraw given amount of cash from an CashMachine. The exact
-     * amount of cash available for withdrawal is subject to CashMachine implementation.
+     * Allows user to withdraw given amount of cash from a cash machine.
+     * The exact amount of cash available for withdrawal is subject to
+     * cash machine implementation.
      *
      * @param amount amount of cash to withdraw
      */
     SortedMap<Integer, Integer> withdraw(int amount);
 
     /**
-     * Gets total amount of cash available in CashMachine.
+     * Gets total amount of cash available in cash machine.
      *
      * @return total amount of cash
      */
