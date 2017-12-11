6647db55a5f37701429571f8f9aae26e4c543fc6
ifqthenp <ifqthenp@gmail.com>
2017-12-11 04:22:46 +0000

    Add default amount constant

--- hw_07_atm/src/main/java/ru/otus/classes/Denominations.java
+++ hw_07_atm/src/main/java/ru/otus/classes/Denominations.java
@@ -7,6 +7,8 @@ public enum Denominations
 {
     HUNDRED(100), FIFTY(50), TWENTY(20), TEN(10), FIVE(5);
 
+    private static final int DEFAULT_AMOUNT = 10;
+
     private Integer denomination;
 
     Denominations(int value)
@@ -17,5 +19,10 @@ public enum Denominations
     public Integer getValue()
     {
         return this.denomination;
+    }
+
+    public static int getDefaultAmount()
+    {
+        return DEFAULT_AMOUNT;
     }
 }
